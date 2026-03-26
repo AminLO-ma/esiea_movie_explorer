d84ddf7b6ab80f4d61e988df631a79cca8181502
Amine EL OUAHBI <mohamed-amine.elouahbi@inovatel.com>
2026-03-26 15:18:18 +0100

    init: setup projet movie explorer

---
+++ android/app/src/main/kotlin/com/example/elouahbi_mohamed_amine_movie_explorer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.elouahbi_mohamed_amine_movie_explorer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/services/movie_service.dart
@@ -0,0 +1,7 @@
+import 'package:http/http.dart' as http;
+
+const String _apiKey = String.fromEnvironment('TMDB_API_KEY');
+
+const String _baseUrl = "https://api.themoviedb.org/3";
+
+class MovieService {}
